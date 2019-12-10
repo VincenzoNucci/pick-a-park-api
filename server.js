58b5eec6e50273ce77c54f12c02b20c6ccd63410
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2019-12-10 13:03:29 +0100

    serving html home page

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const db = require('./db/index.js');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 //Import routes
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 const requestRoute = require('./routes/request');
+const homeRoute = require('./routes/home');
 
 dotenv.config();
 
 var port = process.env.PORT || 3000
+
+//Init EJS
+app.set('view engine', 'ejs');
 
 //Connect to DB
 db.connect().then(() => console.log('Connected to DB!'));
@@ -25,6 +30,9 @@ app.use(cors());
 app.use('/api/user', authRoute);
 app.use('/api/post', postRoute);
 app.use('/api/request', requestRoute);
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/wwwroot/views/home/index.html'));
+});
 
 app.listen(port, () => {
     console.log('Server Up and running!');
